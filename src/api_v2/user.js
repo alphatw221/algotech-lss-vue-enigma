72719f9b68b2e2783ecd34a249ee16731f3ac4a5
asdf50003206 <asdf500032061@gmail.com>
2022-05-25 17:00:43 +0800

    add seller login api

--- src/api_v2/user.js
+++ src/api_v2/user.js
@@ -8,3 +8,13 @@ export const buyer_login_with_facebook = data =>{
 export const buyer_login_with_google = data =>{
     return axiosInstance.post(`/api/v2/buyer/login/google/`,data);
 }
+
+
+
+export const seller_login_with_facebook = data =>{
+    return axiosInstance.post(`/api/v2/seller/login/facebook/`,data);
+}
+
+export const seller_login_with_google = data =>{
+    return axiosInstance.post(`/api/v2/seller/login/google/`,data);
+}
