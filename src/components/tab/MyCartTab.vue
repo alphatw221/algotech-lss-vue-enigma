60138dd4bc56e38cc6a856f529845a00875018a9
root <root@LAPTOP-5LMCTULN.localdomain>
2022-06-01 15:28:06 +0800

    update format

--- src/components/tab/MyCartTab.vue
+++ src/components/tab/MyCartTab.vue
@@ -8,6 +8,7 @@
     </div>
     <div class="col-span-12 2xl:col-start-8 2xl:col-span-5 lg:col-start-8 lg:col-span-5">
         <OrderSummary :page_type="'step1'"> </OrderSummary>
+        <AddItemMoadal :product="store.products"></AddItemMoadal>
     </div>
     </div>
     <div class="box grid grid-cols-12 gap-4 ml-4 mr-4">
@@ -25,6 +26,7 @@
 <script setup>
 import OrderSummary from "@/components/box/OrderSummary.vue";
 import ShoppingCartTable from "@/components/table/ShoppingCartTable.vue";
+import AddItemModal from "@/components/modal/AddItemModal.vue";
 
 import { computed, onMounted, ref, watch } from "vue";
 import { useShoppingCartStore } from "@/stores/lss-shopping-cart";
@@ -36,25 +38,31 @@ const router = useRouter();
 
 const store = useShoppingCartStore(); 
 
-const tableColumns =ref( [
-        { name: " ", key: "image" },
-        { name: "Product", key: "product" },
-        { name: "Q'TY", key: "qty" },
-        { name: "Price", key: "price" },
-        { name: "Subtotal", key: "subtotal" },
-        { name: " ", key: "remove" },
-      ])
+const tableColumns = ref( [
+    { name: " ", key: "image" },
+    { name: "Product", key: "product" },
+    { name: "Q'TY", key: "qty" },
+    { name: "Price", key: "price" },
+    { name: "Subtotal", key: "subtotal" },
+    { name: " ", key: "remove" },
+])
 
 onMounted(()=>{
-  console.log(router.params)
-    // retrieve_pre_order(router.params.pre_order_id)
-    //   .then(
-    //     res => {
-    //       for (const [key, value] of Object.entries(res.data.products)) {
-    //           store.products.push(value)
-
-    //       }
-    //     }
-    //   )
+    retrieve_pre_order(route.params.pre_order_id)
+      .then(
+        res => {
+            console.log(res.data)
+            for (const [key, value] of Object.entries(res.data.products)) {
+                value.image = import.meta.env.VITE_APP_IMG_URL + value.image
+                store.products.push(value)
+            }
+            store.orderSummary = {
+                'subtotal': res.data.subtotal,
+                'total': res.data.total,
+                'deliveryCharge': res.data.shipping_cost,
+                'adjustPrice': res.data.adjust_price,
+            }
+        }
+      )
 })
 </script>
