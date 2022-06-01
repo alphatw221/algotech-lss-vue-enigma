a8f18263d223963c3c19a07c9c4d6e3e85076c46
e12mv2mgh <jeremychou@accoladeglobal.net>
2022-06-01 10:36:48 +0800

    step1

--- src/views/shoppingcart/Main.vue
+++ src/views/shoppingcart/Main.vue
@@ -67,7 +67,7 @@
                   </ShoppingCartTable>
                 </div>
                 <div class="col-span-12 2xl:col-start-8 2xl:col-span-5 lg:col-start-8 lg:col-span-5">
-                  <OrderSummary :page_type="'stap1'"> </OrderSummary>
+                  <OrderSummary :page_type="'step1'"> </OrderSummary>
                 </div>
               </div>
               <div class="box grid grid-cols-12 gap-4 ml-4 mr-4">
