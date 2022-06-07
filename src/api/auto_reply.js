3dfa9a11c803a961737af0f7c21f59ebe73953a9
Nick LIen <nicklien@accoladeglobal.net>
2022-06-07 22:05:21 +0800

    finish auto reply adjust code

--- src/api/auto_reply.js
+++ src/api/auto_reply.js
@@ -3,8 +3,8 @@ import { createAxiosWithBearer } from "@/libs/axiosClient";
 export const auto_response_list = () => {
     return createAxiosWithBearer().get(`/api/auto_response/list/`)
 } 
-export const create_auto_response = (platform_name,platform_id) => {
-    return createAxiosWithBearer().post(`/api/auto_response/create/${platform_name}/${platform_id}/`)
+export const create_auto_response = (platform_name,platform_id, data) => {
+    return createAxiosWithBearer().post(`/api/auto_response/create/${platform_name}/${platform_id}/`, data)
 }
 export const update_auto_response = (id, data) => {
     return createAxiosWithBearer().put(`/api/auto_response/${id}/update/`, data)
