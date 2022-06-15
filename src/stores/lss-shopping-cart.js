762609d3d1a134fc1847c117d008fe0b6b9b0f61
asdf50003206 <asdf500032061@gmail.com>
2022-06-15 11:58:21 +0800

    update shipping address

--- src/stores/lss-shopping-cart.js
+++ src/stores/lss-shopping-cart.js
@@ -4,7 +4,13 @@ export const useShoppingCartStore = defineStore("buyerShoppingCart", {
 	state: () => ({
 		// preOrder:ref({}),
 		openTab: ref(1),
-		order: ref({}),
+		order: ref({
+			subtotal:0,
+			adjust_title:'',
+			adjust_price:0,
+			shipping_cost:0,
+			total:0
+		}),
 		orderDetail: ref({}),
 		// orderSummary:ref({}),
 		contact_info: ref({
@@ -20,23 +26,23 @@ export const useShoppingCartStore = defineStore("buyerShoppingCart", {
 		shipping_info: ref({
 			shipping_option:"",
 			shipping_method: "pickup",
-			shipping_first_name: "",
-			shipping_last_name: "",
-			shipping_email: "",
-			shipping_phone: "",
-			shipping_gender: "",
-			shipping_company: "",
-			shipping_postcode: "",
-			shipping_region: "",
-			shipping_location: "",
-			shipping_address_1: "",
-			shipping_address_2: "",
-			shipping_status: "",
-			shipping_details: "",
-			shipping_remark: "",
-			shipping_date: null,
-			shipping_time: null,
-			pickup_address:""
+			// shipping_first_name: "",
+			// shipping_last_name: "",
+			// shipping_email: "",
+			// shipping_phone: "",
+			// shipping_gender: "",
+			// shipping_company: "",
+			// shipping_postcode: "",
+			// shipping_region: "",
+			// shipping_location: "",
+			// shipping_address_1: "",
+			// shipping_address_2: "",
+			// shipping_status: "",
+			// shipping_details: "",
+			// shipping_remark: "",
+			// shipping_date: null,
+			// shipping_time: null,
+			// pickup_address:""
 		})
 
 
