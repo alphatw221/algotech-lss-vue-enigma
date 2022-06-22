fb0bdfff986b22b09f4177da8dfc92e827e98f8b
e12mv2mgh <jeremychou@accoladeglobal.net>
2022-06-22 15:58:12 +0800

    update api

--- src/api_v2/order_product.js
+++ src/api_v2/order_product.js
@@ -8,3 +8,10 @@ export const buyer_delete_order_product = (order_product_id) =>{
 export const buyer_update_order_product = (order_product_id, qty) =>{
     return createAxiosWithBearer().put(`/api/v2/order-product/${order_product_id}/buyer/update/?qty=${qty}`)
 }
+
+export const seller_delete_product = (pre_order_id,order_product_id) => {
+    return createAxiosWithBearer().get(`/api/v2/order-product/${pre_order_id}/seller/delete/?order_product_id=${order_product_id}`)
+}
+export const seller_update_product = (pre_order_id,order_product_id,qty) => {
+    return createAxiosWithBearer().get(`/api/v2/order-product/${pre_order_id}/seller/update/?order_product_id=${order_product_id}&qty=${qty}`)
+}
