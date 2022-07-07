4bdd95434233f188d03e137994e564d10db349dd
e12mv2mgh <jeremychou@accoladeglobal.net>
2022-07-07 15:21:08 +0800

    update i18n

--- src/locales/zh-hant/index.js
+++ src/locales/zh-hant/index.js
@@ -1,4 +1,20 @@
 import stock from './stock.json'
+import global from './global.json'
+import settings from './settings.json'
+import shopping_cart from './shopping_cart.json'
+import components from './components.json'
+import assign_product from './campaign/assign_product.json'
+import create_edit from './campaign/create_edit.json'
+import liveshow from './campaign/liveshow.json'
+import campaign from './campaign/index.json'
 export default {
-    stock
+    stock,
+    global,
+    settings,
+    shopping_cart,
+    components,
+    assign_product,
+    create_edit,
+    liveshow,
+    campaign
 }
