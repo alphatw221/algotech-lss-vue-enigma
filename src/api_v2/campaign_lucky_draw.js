78fef7731427b31b1daad6c8a8dede6b86f2965a
root <root@LAPTOP-5LMCTULN.localdomain>
2022-07-08 12:01:26 +0800

    refacotr: lucky draw flow

--- src/api_v2/campaign_lucky_draw.js
+++ src/api_v2/campaign_lucky_draw.js
@@ -3,3 +3,19 @@ import { createAxiosWithBearer , Paginator} from "@/libs/axiosClient";
 export const create_campapign_lucky_draw = (campaign_id, data) => {
     return createAxiosWithBearer().post(`/api/v2/campaign-luckydraw/${campaign_id}/create/`, data)
 }
+
+export const list_campapign_lucky_draw_animation = () => {
+    return createAxiosWithBearer().get('/api/v2/campaign-luckydraw/list/animation/')
+}
+
+export const list_campaign_lucky_draw = (campaign_id) => {
+    return createAxiosWithBearer().get(`/api/v2/campaign-luckydraw/${campaign_id}/list/`)
+}
+
+export const retrieve_campaign_lucky_draw = (lucky_draw_id) => {
+    return createAxiosWithBearer().get(`/api/v2/campaign-luckydraw/${lucky_draw_id}/retrieve/`)
+}
+
+export const draw_campaign_lucky_draw = (lucky_draw_id) => {
+    return createAxiosWithBearer().post(`/api/v2/campaign-luckydraw/${lucky_draw_id}/draw/`)
+}
