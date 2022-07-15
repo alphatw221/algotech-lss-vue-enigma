5953e10a0c2421f145592991f5e0ed0298b66926
Tien <tienhuifeng@algotech.app>
2022-07-15 12:02:34 +0800

    layout

--- src/views/create-campaign/Main.vue
+++ src/views/create-campaign/Main.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="flex flex-col overflow-auto sm:overflow-hidden h-screen sm:h-[100%] text-[16px] h-fit">
-		<div class="flex items-center px-20 pt-5 pb-4 intro-y">
+    <div class="flex flex-col h-[100%] text-[16px]">
+		<div class="flex items-center sm:px-20 pt-5 pb-4 intro-y">
 			<h2 class="text-xl sm:text-2xl font-medium mx-auto sm:mx-0">Create Campaign</h2>
 		</div>
-		<div class="box grid grid-cols-12 gap-4 p-5 intro-y lg:mx-20 lg:px-40 px-10 py-10">
+		<div class="box grid grid-cols-12 gap-4 p-5 intro-y lg:mx-20 lg:px-40">
     <!-- <div class="flex flex-col gap-5 text-[16px] h-fit">
 		<span class="mt-3 ml-5 text-xl sm:ml-0 sm:mt-5"> Create Campaign </span>
 		<div class="grid grid-cols-12 gap-1 px-5 py-5 sm:p-8 intro-y box sm:gap-5 -z-50"> -->
