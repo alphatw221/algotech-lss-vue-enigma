39586707aaf6e533ad438cda7891e46ef016a0cd
root <root@LAPTOP-5LMCTULN.localdomain>
2022-07-18 15:52:56 +0800

    refactor: i18n change locale by subscription

--- src/locales/i18n.js
+++ src/locales/i18n.js
@@ -10,14 +10,13 @@ import vi from './vi/index.js'
 const i18n = createI18n({
   legacy: false,
   locale:'en',
-  // locale:'zh_hant',
   fallbackLocale: 'en',
   globalInjection: true,
   messages:{
       //語言包
       "zh_hant": zh,
       "en": en,
-      "vi":vi
+      "vi": vi
   }
 })
 
