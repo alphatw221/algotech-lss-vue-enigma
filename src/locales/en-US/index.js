e7d31e99cbad1829352c464bb79fe8e6d4abca64
e12mv2mgh <jeremychou@accoladeglobal.net>
2022-07-18 11:16:15 +0800

    revision campaign langusge json

--- src/locales/en-US/index.js
+++ src/locales/en-US/index.js
@@ -3,9 +3,7 @@ import global from './global.json'
 import settings from './settings.json'
 import shopping_cart from './shopping_cart.json'
 import components from './components.json'
-import create_edit from './campaign/create_edit.json'
-import campaign_live from './campaign/liveshow.json'
-import campaign from './campaign/index.json'
+import campaign from './campaign.json'
 import manage_order from './manage_order.json'
 import order_detail from './order_detail.json'
 export default {
@@ -14,8 +12,6 @@ export default {
     settings,
     shopping_cart,
     components,
-    create_edit,
-    campaign_live,
     campaign,
     manage_order,
     order_detail
