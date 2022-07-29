d55aed2d10d40b0e518a0a548a892feadcfccb6a
NiveaC <a26520008@yahoo.com.tw>
2022-07-29 17:58:17 +0800

    多國語言_簡中

---
+++ src/locales/zh_hans/index.js
@@ -0,0 +1,40 @@
+import global from './global.json'
+import stock from './views/stock.json'
+import settings from './views/settings.json'
+import shopping_cart from './views/shopping_cart.json'
+import manage_order from './views/manage_order.json'
+import order_detail from './views/order_detail.json'
+import order_history from './views/order_history.json'
+import campaign_list from './views/campaign_list.json'
+import campaign_live from './views/campaign_live.json'
+import create_campaign from './views/create_campaign.json'
+import edit_campaign from './views/edit_campaign.json'
+import lucky_draw from './views/lucky_draw.json'
+import auto_reply from './views/auto_reply.json'
+import assign_product from './views/assign_product.json'
+import layout from './views/layout.json'
+import profile from './views/profile.json'
+import edit_campaign_product from './views/edit_campaign_product.json'
+import change_plan from './views/change_plan.json'
+export default {
+    global,
+
+    campaign_list,
+    campaign_live,
+    create_campaign,
+    edit_campaign,
+    lucky_draw,
+
+    manage_order,
+    order_detail,
+    order_history,
+    settings,
+    shopping_cart,
+    stock,
+    auto_reply,
+    assign_product,
+    layout,
+    edit_campaign_product,
+    change_plan,
+    profile
+}
