113b1391a3664d1087f0e27899a82ef19009c0a3
asdf50003206 <asdf500032061@gmail.com>
2022-08-01 12:54:39 +0800

    add hitpay

--- src/api_v2/payment.js
+++ src/api_v2/payment.js
@@ -4,6 +4,11 @@ import { axiosInstance, createAxiosWithBearer } from "@/libs/axiosClient";
 const get_stripe_gateway = (order_oid) => {
     return axiosInstance.get(`/api/v2/payment/stripe/gateway/?order_oid=${order_oid}`)
 }
+
+const get_hitpay_gateway = (order_oid) => {
+    return axiosInstance.get(`/api/v2/payment/hitpay/gateway/?order_oid=${order_oid}`)
+}
+
 const get_first_data_credential = (order_oid) => {
     return axiosInstance.get(`/api/v2/payment/first_data/credential/?order_oid=${order_oid}`)
 }
@@ -11,5 +16,6 @@ const get_first_data_credential = (order_oid) => {
 
 export const paymentEndPoints={
     'getStripeGateway':get_stripe_gateway,
-    'getFirstDataCredential':get_first_data_credential
+    'getFirstDataCredential':get_first_data_credential,
+    'getHitpayGateway':get_hitpay_gateway
 }
