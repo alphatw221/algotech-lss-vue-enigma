15b5c27b78a2ffc17cd8055cd1d475fba1eace0e
asdf50003206 <asdf500032061@gmail.com>
2022-08-01 16:40:11 +0800

    update edit / create campaign

--- src/views/settings/Localization.vue
+++ src/views/settings/Localization.vue
@@ -9,7 +9,8 @@
                 <div class="mr-3 text-lg sm:text-xl font-medium"> {{$t("settings.localization.country")}}  : </div>
                 <div class="flex my-auto" v-if="layoutStore.userInfo.user_subscription "> {{  $t(`settings.localization.countries.${layoutStore.userInfo.user_subscription.country}`)||'' }}</div>
             </div>
-            <div class="flex my-3 mt-5 form-label text-base font-medium">
+
+            <!-- <div class="flex my-3 mt-5 form-label text-base font-medium">
                 <div> {{$t("settings.localization.currency_symbol")}} </div>
             </div>
             <div class="flex my-1">
@@ -18,7 +19,8 @@
                             }" class="w-5/6">
                     <option :value="option.value" v-for="(option,index) in currencySymbols" :key="index">{{option.text}}</option>
                 </TomSelect>
-            </div>
+            </div> -->
+
             <div class="flex my-3 mt-5 form-label text-base font-medium">
                 <div class="mr-5"> {{$t("settings.localization.seller_language")}}</div>
             </div> 
@@ -29,7 +31,8 @@
                     <option :value="option.value" v-for="(option,index) in languages" :key="index">{{$t(`settings.localization.languages.${option.value}`)}}</option>
                 </TomSelect>
             </div>
-            <div class="flex my-3 mt-5 form-label text-base font-medium">
+
+            <!-- <div class="flex my-3 mt-5 form-label text-base font-medium">
                 <div class="mr-5"> {{$t("settings.localization.buyer_language")}}</div>
             </div>
             <div class="flex my-1">
@@ -38,9 +41,9 @@
                             }" class="w-5/6">
                     <option :value="option.value" v-for="(option,index) in languages" :key="index">{{$t(`settings.localization.languages.${option.value}`)}}</option>
                 </TomSelect>
-            </div>
+            </div> -->
 
-            <div class="flex my-3 mt-5 form-label text-base font-medium">
+            <!-- <div class="flex my-3 mt-5 form-label text-base font-medium">
                 <div class="mr-5"> {{$t("settings.localization.price_unit")}}</div>
             </div>
 
@@ -48,16 +51,16 @@
                 <TomSelect v-model="data.price_unit" :options="{placeholder: $t('settings.localization.choose_price_unit')}" class="w-5/6">
                     <option :value="option.value" v-for="(option,index) in priceUnitOptions" :key="index">{{$t(`settings.localization.priceOptions.${option.key}`)}}</option>
                 </TomSelect>
-            </div>
+            </div> -->
 
-            <div class="flex my-3 mt-5 form-label text-base font-medium">
+            <!-- <div class="flex my-3 mt-5 form-label text-base font-medium">
                 <div class="mr-5"> {{$t("settings.localization.decimal_places")}}</div>
             </div>
             <div class="flex my-1">
                 <TomSelect v-model="data.decimal_places" :options="{placeholder: $t('settings.localization.choose_decimal_places')}" class="w-5/6">
                     <option :value="option.value" v-for="(option,index) in decimalOptions" :key="index">{{option.text}}</option>
                 </TomSelect>
-            </div>
+            </div> -->
 
             
 
@@ -81,57 +84,48 @@ const app_i18n = getCurrentInstance().appContext.config.globalProperties.$i18n
 
 const countries = ref({'PH':'Philippines','SG':'Singapore','ID':'Indonesia','IN':'India','TW':'Taiwan'})
 
-const currencySymbols = ref([
-    {value:'USD',text:'USD'},
-    {value:'NTD',text:'NTD'},
-    {value:'SGD',text:'SGD'},
-    {value:'IDR',text:'IDR'},
-    {value:'PHP',text:'PHP'},
-    {value:'MYR',text:'MYR'},
-    {value:'VND',text:'VND'},
-    {value:'RMB',text:'RMB'},
-    {value:'KHR',text:'KHR'},
-    {value:'AUD',text:'AUD'},
-    {value:'HKD',text:'HKD'}])
+// const currencySymbols = ref([
+//     {value:'USD',text:'USD'},
+//     {value:'NTD',text:'NTD'},
+//     {value:'SGD',text:'SGD'},
+//     {value:'IDR',text:'IDR'},
+//     {value:'PHP',text:'PHP'},
+//     {value:'MYR',text:'MYR'},
+//     {value:'VND',text:'VND'},
+//     {value:'RMB',text:'RMB'},
+//     {value:'KHR',text:'KHR'},
+//     {value:'AUD',text:'AUD'},
+//     {value:'HKD',text:'HKD'}])
 
-const priceUnitOptions = ref([
-    {key:'1',value:'1'},
-    {key:'1000',value:'1000'},
-    {key:'1000000',value:'1000000'},
-])
+// const priceUnitOptions = ref([
+//     {key:'1',value:'1'},
+//     {key:'1000',value:'1000'},
+//     {key:'1000000',value:'1000000'},
+// ])
 
 const languages = ref([
     {value:'en',text:'English'},
-    // {value:'zh_hans',text:'Chinese-simplify'},
     {value:'zh_hant',text:'Chinese-tranditional'},
-    // {value:'id',text:'Indonesian'}
 ])
 
-const decimalOptions = ref([
-    {value:'2',text:'0.01'},
-    // {value:'1',text:'0.1'},
-    {value:'0',text:'1'},
-    // {value:'-1',text:'10'},
-    // {value:'-2',text:'100'},
-    // {value:'-3',text:'1000'}
-    ])
+// const decimalOptions = ref([
+//     {value:'2',text:'0.01'},
+//     {value:'0',text:'1'},
+//     ])
 
 const data = ref({currency:'USD', lang:'en', buyer_lang:'en', decimal_places:'2', price_unit:'1'})
+
 onMounted(()=>{
-    console.log(layoutStore.userInfo)
+
     if(!layoutStore.userInfo.user_subscription) return
-    data.value.currency = layoutStore.userInfo.user_subscription.currency
     data.value.lang = layoutStore.userInfo.lang
-    data.value.buyer_lang = layoutStore.userInfo.user_subscription.buyer_lang
-    data.value.decimal_places = layoutStore.userInfo.user_subscription.decimal_places.toString()
-    // data.value.price_unit = layoutStore.userInfo.user_subscription.price_unit
 })
 
 const clean =() =>{
-    data.value.currency = layoutStore.userInfo.user_subscription.currency
+    // data.value.currency = layoutStore.userInfo.user_subscription.currency
     data.value.lang = layoutStore.userInfo.lang
-    data.value.buyer_lang = layoutStore.userInfo.user_subscription.buyer_lang
-    data.value.decimal_places = layoutStore.userInfo.user_subscription.decimal_places.toString()
+    // data.value.buyer_lang = layoutStore.userInfo.user_subscription.buyer_lang
+    // data.value.decimal_places = layoutStore.userInfo.user_subscription.decimal_places.toString()
 }
 
 const save = ()=>{
