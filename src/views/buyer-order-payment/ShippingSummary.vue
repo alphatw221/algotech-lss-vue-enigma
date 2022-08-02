c4804ec338fac9f9f82cc7d47bcda561f4e9bb9f
Jeremy <jeremychou@accoladeglobal.net>
2022-08-02 13:52:24 +0800

    fix pickup address display bug

--- src/views/buyer-order-payment/ShippingSummary.vue
+++ src/views/buyer-order-payment/ShippingSummary.vue
@@ -20,7 +20,7 @@
             <div class="flex mt-4 mr-auto">{{$t('shopping_cart.payment.pickup')}}</div>
             <div class="flex mt-4"> 
                 <span class="mr-10">{{ store.order.shipping_option }}</span>
-                <div >{{store.order.pickup_address}}</div>
+                <div >{{store.order.campaign.meta_logistic.pickup_options[store.order.shipping_option_index].address}}</div>
             </div>
             
         </template>
