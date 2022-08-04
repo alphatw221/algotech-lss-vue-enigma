b99c701f116b223cf4d7c6d6c5806d55dbf39a06
root <root@LAPTOP-5LMCTULN.localdomain>
2022-08-04 14:56:57 +0800

    feat: quiz game prototype

--- src/api_v2/campaign_quiz_game.js
+++ src/api_v2/campaign_quiz_game.js
@@ -5,16 +5,20 @@ export const create_campaign_quiz_game = (campaign_id, data) => {
     return createAxiosWithBearer().post(`/api/v2/campaign-quizgame/${campaign_id}/create/`, data)
 }
 
-export const update_campaign_quiz_game = (quizgame_id, data) => {
-    return createAxiosWithBearer().put(`/api/v2/campaign-quizgame/${quizgame_id}/update/`, data)
+export const update_campaign_quiz_game = (quizgame_bundle_id, data) => {
+    return createAxiosWithBearer().put(`/api/v2/campaign-quizgame/${quizgame_bundle_id}/update/`, data)
+}
+
+export const delete_campaign_quiz_game_bundle = (quizgame_bundle_id) => {
+    return createAxiosWithBearer().delete(`/api/v2/campaign-quizgame/${quizgame_bundle_id}/delete/bundle/`)
 }
 
 export const delete_campaign_quiz_game = (quizgame_id) => {
-    return createAxiosWithBearer().delete(`/api/v2/campaign-quizgame/${quizgame_id}/delete/`)
+    return createAxiosWithBearer().delete(`/api/v2/campaign-quizgame/${quizgame_id}/delete/quiz-game/`)
 }
 
-export const retrieve_campaign_quiz_game = (quizgame_id) => {
-    return createAxiosWithBearer().get(`/api/v2/campaign-quizgame/${quizgame_id}/retrieve/`)
+export const retrieve_campaign_quiz_game = (quizgame_bundle_id) => {
+    return createAxiosWithBearer().get(`/api/v2/campaign-quizgame/${quizgame_bundle_id}/retrieve/`)
 }
 
 export const list_campaign_quiz_game = (campaign_id) => {
@@ -27,4 +31,8 @@ export const start_campaign_quiz_game = (quizgame_id) => {
 
 export const stop_campaign_quiz_game = (quizgame_id) => {
     return createAxiosWithBearer().get(`/api/v2/campaign-quizgame/${quizgame_id}/stop/`)
+}
+
+export const result_campaign_quiz_game = (quizgame_bundle_id) => {
+    return createAxiosWithBearer().post(`/api/v2/campaign-quizgame/${quizgame_bundle_id}/result/`)
 }
