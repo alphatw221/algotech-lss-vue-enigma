988dc1478f13569c16050b75a82f39f48de61f50
asdf50003206 <asdf500032061@gmail.com>
2022-08-17 19:23:41 +0800

    register page language detect

--- src/views/seller-registration/Main.vue
+++ src/views/seller-registration/Main.vue
@@ -92,8 +92,10 @@ const route = useRoute();
 const router = useRouter();
 
 onBeforeMount (()=>{document.querySelector('body').setAttribute('style', 'padding-left: 0;')} ) 
-onBeforeMount (()=>{useI18n.global.locale.value = route.query.language || 'en'} )     
+  
 onMounted(()=>{
+
+    useI18n.global.locale.value = route.query.language || 'en'
     loadScript("https://js.stripe.com/v3/",()=>{console.log("stripe SDK loaded") })
     // registerationStore.country = route.query.language
 })
