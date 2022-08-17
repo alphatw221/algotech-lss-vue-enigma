0fa33dc324dd1c9339d7b400bb136930d4ed15c3
asdf50003206 <asdf500032061@gmail.com>
2022-08-17 18:31:06 +0800

    fix post id modal

--- src/api_v2/instagram.js
+++ src/api_v2/instagram.js
@@ -1,5 +1,5 @@
 import { instagramAxios } from "@/libs/axiosClient";
-import { createAxiosWithBearer } from "@/libs/axiosClient";
+import { createAxiosWithBearer, createAxiosWithBearerWithoutInterceptor } from "@/libs/axiosClient";
 
 export const get_ig_live_media = (pageID, pageToken) => {
     return instagramAxios(pageToken).get(`/${pageID}/live_media?fields=id,media_url,username`)
