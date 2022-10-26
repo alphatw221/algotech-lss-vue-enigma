2e6f979f994cc305b866b239c45823e845c725ca
林奕學 <lin@linyixuedeMac-mini.local>
2022-10-26 13:04:06 +0800

    update

--- src/stores/lss-campaign-detail.js
+++ src/stores/lss-campaign-detail.js
@@ -3,11 +3,15 @@ import { ref } from "vue"
 
 export const useCampaignDetailStore = defineStore("CampaignDetail", {
 	state: () => ({
+
+		campaignProductCampaignID:ref(null),//indecate campaign_id of campaign_products
 		campaignProducts:ref([]),
 		campaignProductDict:ref({}),
+
 		showInstantlyAddProductModal:ref(false),
 		showAddProductFromStockModal:ref(false),
 		showEditCampaignProductModal:ref(false),
+		
 		incomingOrders:ref([]),
 		incomingOrdersDict:ref({}),
 		campaign:ref({})
