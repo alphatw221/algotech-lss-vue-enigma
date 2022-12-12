0adf89941be12ee179d513ef6d083f62e0ab5718
TIEN <tienhuifeng@accoladeglobal.net>
2022-12-12 13:18:25 +0800

    pick up pay

--- src/views/buyer-order-payment/payment-methods/PickupPay.vue
+++ src/views/buyer-order-payment/payment-methods/PickupPay.vue
@@ -2,33 +2,39 @@
 
 <AccordionItem  class="mx-5 " >
         <Accordion class="bg-primary rounded-t-lg" >
-            <div class="text-white mx-5" > {{payment.name}} </div>
+            <div class="text-white mx-5" > 貨到付款 </div>
         </Accordion>
         <AccordionPanel class="text-slate-600 dark:text-slate-500 leading-relaxed border-2 border-secondary" >
-            <div  v-for="storeInfo in props.payment" :key="storeInfo" class="flex flex-col">
-                <p>
-                    {{store.order.shipping_option_data[storeInfo.key]}}
-                </p>
+            <div v-if="store.order.campaign" class="grid grid-cols-3 justify-center text-left mx-20 text-[16px]">
+                <lable class="col-span-3 col-start-1 text-center my-3 text-[20px]"> 超商資訊:</lable>
+                <p> 門市名稱: </p>
+                <p class="col-span-2">{{store.order.shipping_option_data.cvs_store_name}}</p>
+                <p> 地址: </p>
+                <p class="col-span-2">{{store.order.shipping_option_data.cvs_address}}</p>
+                <p> 店號: </p>
+                <p class="col-span-2">{{store.order.shipping_option_data.cvs_store_id}}</p>
+                <template v-if="store.order.shipping_option_data.cvs_telephone">
+                    <p > 門市名稱: </p>
+                    <p>{{store.order.shipping_option_data.cvs_telephone}}</p>
+                </template>   
+                
+                <button class="col-span-3 w-fit btn btn-primary rounded-lg mt-10 mb-3 ml-auto" @click="cash_on_delivery()">
+                    貨到付款  
+                </button>
             </div>
         </AccordionPanel>
-
     </AccordionItem>
-    <button class="w-fit btn btn-rounded-primary" @click="cash_on_delivery()">
-        test ecpay
-    </button>
 </template>
 
 <script setup>
 import {onMounted, computed, watch, ref} from "vue"
 import { useRoute, useRouter } from "vue-router";
 import { useLSSBuyerOrderStore } from "@/stores/lss-buyer-order";
+import { useLSSBuyerLayoutStore } from "@/stores/lss-buyer-layout";
 import {buyer_cash_on_delivery} from "@/api_v2/order" 
 
 const store = useLSSBuyerOrderStore();
-
-const props = defineProps({
-    payment: Object,
-});
+const layoutStore = useLSSBuyerLayoutStore()
 
 const route = useRoute();
 const router = useRouter();
@@ -37,6 +43,8 @@ const cash_on_delivery = () =>{
     buyer_cash_on_delivery(route.params.order_oid,{'is_collection': 'Y'}).then(
     res=>{
         console.log(res.data)
+        layoutStore.notification.showMessageToast(res.data.RtnMsg)
+        router.push(`/buyer/order/${route.params.order_oid}/confirmation`)
     }
   )
   
