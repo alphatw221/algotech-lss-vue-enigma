6c0a94835bb1e6a7d40245e955edd5c9b5587449
TIEN <tienhuifeng@accoladeglobal.net>
2022-12-29 17:33:12 +0800

    tiktok page finished

--- src/views/buyer-search-tiktok-cart/Main.vue
+++ src/views/buyer-search-tiktok-cart/Main.vue
@@ -6,11 +6,15 @@
                 src="https://assets10.lottiefiles.com/packages/lf20_vIyvPR.json" loop background="transparent"  speed="1"  style="width: 300px; height: 300px;"   autoplay></lottie-player>
         </div>
         <template v-if="!showAnimate">
-            <div class="flex flex-row lg:gap-10 justify-center py-10"> 
+            <div class="flex flex-row lg:gap-10 justify-center pt-2 pb-5 lg:py-10"> 
                 <!-- <img src="@/assets/images/lss-img/kuaimai-avatar.jpeg" class="h-[80px] lg:h-[130px] rounded-xl lg:ml-20"/>  -->
-                <p class="my-auto text-[1.8rem] lg:text-[2rem]"> 買買買</p>
-                <LoadingIcon icon="loading_right" class="w-[80px] my-auto lg:w-[120px]"/>
-                <img src="@/assets/images/lss-img/tiktok_black_bg.png" class="h-[80px] lg:h-[150px] my-auto"/>
+                <img src="@/assets/images/lss-img/tiktok_black_bg.png" class="h-[70px] lg:h-[150px] my-auto z-10"/>
+                <div class="relative w-[100px] lg:w-[200px]"> 
+                    <div class="loading"> 
+                        <div class="bullet"></div><div class="bullet"></div><div class="bullet"></div>
+                    </div>
+                </div>
+                <p class=" my-auto py-2 font-medium text-[1.2rem] lg:text-[1.8rem] z-10 bg-slate-100"> {{campaign?.tiktok_campaign?.username}}</p>
             </div>
             <div class="h-full w-full xl:w-3/4 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[16px] font-medium text-primary">
                 <div
@@ -21,11 +25,12 @@
                         <div class="flex flex-col gap-3 justify-center mx-auto lg:ml-16 relative"> 
                             <label class="text-xl font-medium"> 進入您的專屬購物車</label>
                             <label class="text-lg font-bold mt-3"> 方式一</label>
-                            <button class="tiktok-login-btn shadow-lg"><a class="text-white hover:text-white" :href="`${baseURL}/api/v2/tiktok/${route.params.campaign_id}/cart?message`"> 登入TikTok帳號 </a></button>
+
+                            <a class="tiktok-login-btn shadow-lg flex justify-center" :href="`${serverDomain}/api/v2/tiktok/${route.params.campaign_id}/cart?message`"><p class="my-auto">登入TikTok帳號</p>  </a>
                             <label class="text-lg font-bold mt-3"> 方式二</label>
-                            <label class="mx-auto">輸入您的TikTok使用者名稱（暱稱）</label>
+                            <label class="">輸入您的TikTok使用者名稱（暱稱）</label>
                             <div class="flex flex-row h-[42px]"> 
-                                <input type="text" v-model="customerName" class="rounded-l-lg w-full h-[42px] border-primary ">
+                                <input type="text" v-model="customerName" class="rounded-l-lg w-full h-[42px] border-primary">
                                 <button class="btn btn-primary w-fit px-5 rounded-l-none whitespace-nowrap" @click="searchTiktokCart()">確認</button>
                             </div>
                             <a class="mx-auto underline text-slate-600 my-2" @click="showModal=true"> 找不到暱稱?</a>
@@ -40,8 +45,8 @@
                     </div>
                     <div class="flex w-full h-full lg:p-10 mx-auto ">
                         <div class="flex flex-col gap-3 justify-center mx-auto relative w-[300px]"> 
-                            <label class="text-xl font-medium mb-5 lg:mb-0 lg:absolute top-0 left-0">直接選購商品</label>
-                            <a class="btn btn-primary px-5 w-fit mx-auto hover:text-white"  
+                            <label class="text-xl font-medium mb-5 text-center lg:text-left">直接選購商品</label>
+                            <a class="btn btn-primary px-5 w-fit hover:text-white mx-auto lg:mx-0"  
                                 :href="`${baseURL}/buyer/recaptcha/blank/${route.params.campaign_id}/`"> 前往選購</a>
                             
                         </div>
@@ -86,21 +91,22 @@ import loadScript from '@/libs/loadScript.js'
 import {onMounted, ref} from 'vue'
 import { useRoute, useRouter } from "vue-router";
 import BindTiktokAccountButton from '@/components/button/BindTiktokAccountButton.vue'
-
 import { useLSSBuyerLayoutStore } from '@/stores/lss-buyer-layout';
-
 import {buyer_search_tiktok_cart} from '@/api_v2/cart'
-import { ModalHeader } from '../../global-components/modal';
+import {retrieve_campaign} from '@/api_v2/campaign'
 
+
+const siteKey = import.meta.env.VITE_RECAPTCHA_SITE_KEY
+const baseURL = import.meta.env.VITE_APP_WEB
+const serverDomain = import.meta.env.VITE_APP_ROOT_API
 
 const buyerStore = useLSSBuyerLayoutStore();
 const route = useRoute();
 const router = useRouter();
-const siteKey = import.meta.env.VITE_RECAPTCHA_SITE_KEY
-const baseURL = import.meta.env.VITE_APP_WEB
+
 const showAnimate = ref(true)
 const showModal = ref(false)
-
+const campaign = ref([])
 const recaptchaToken = ref(null)
 const customerName = ref('')
 onMounted(()=>{
@@ -108,6 +114,7 @@ onMounted(()=>{
         showAnimate.value=true
     })
     loadRecaptcha()
+    retrieveCampaign()
 })
 
 const loadRecaptcha = ()=>{
@@ -128,10 +135,19 @@ const searchTiktokCart = ()=>{
         setInterval(()=>{location.reload()},1000);
     })
 }
+
+const retrieveCampaign = ()=>{
+    retrieve_campaign(route.params.campaign_id).then(res=>{
+        console.log(res.data)
+        campaign.value = res.data
+    })
+}
+
 </script>
 
 
-<style scoped>
+<style lang="scss" scoped>
+
 .tiktok-login-btn { 
     border: 1px solid rgb(5, 0, 10);
     background-color: #020003;
@@ -145,12 +161,44 @@ const searchTiktokCart = ()=>{
     font-size: 16px;
 }
 
-.body-middle {
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-    z-index: 999;
-    width: 60px;
-    height: 60px;
+$color: #65a3ff;
+$delay: .15s;
+$size: 15px;
+$bullets: 4;
+
+.loading{
+  position: absolute;
+  top: 45%;
+  left: 50%;
+  
+  .bullet{
+    position: absolute;
+    padding: $size/2;
+    border-radius: 50%;
+    background: $color;
+    animation: animIn 1s ease-in-out 0s infinite;
+    
+    $i: 0s;
+    @for $level from 0 to $bullets {
+      &:nth-child(#{$level + 1}) { animation-delay: $i; }
+      $i: $i + $delay;
+    }
+    
+  }
+}
+        
+@keyframes animIn {
+  0% {
+    transform: translateX(-100px);
+    opacity: 0;
+  }    
+  
+  50%{ opacity: 1; }
+  
+  100%{
+    transform: translateX(100px);
+    opacity: 0;
+  }
 }
 </style>
+
