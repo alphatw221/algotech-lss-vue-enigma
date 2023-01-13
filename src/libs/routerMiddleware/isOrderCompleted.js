ee4a08012797987270f54278bc0b385f75412af4
lin <yihsuehlin@accoladeglobal.com>
2023-01-13 16:44:05 +0800

    update

--- src/libs/routerMiddleware/isOrderCompleted.js
+++ src/libs/routerMiddleware/isOrderCompleted.js
@@ -14,7 +14,7 @@ export default async (to, from)=>{
     const response = await buyer_retrieve_order_state(order_oid)
     if (response.data.payment_status == 'paid' || response.data.payment_method.includes('ecpay')) {
 
-        return `/buyer/order/${order_oid}`
+        return `/buyer/order/${order_oid}/confirmation`
     }
     if (response.status != 200) {
         return from
