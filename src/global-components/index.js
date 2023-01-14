35bff42734d1d85a57c3c04472a8ecdab2a71d8f
TIEN <tienhuifeng@accoladeglobal.net>
2023-01-14 22:44:36 +0800

    delete tom input

--- src/global-components/index.js
+++ src/global-components/index.js
@@ -5,7 +5,6 @@ import Litepicker from "./litepicker/Main.vue";
 import Tippy from "./tippy/Main.vue";
 import TippyContent from "./tippy-content/Main.vue";
 import TomSelect from "./tom-select/Main.vue";
-import TomInput from "./tom-input/Main.vue";
 import TinySlider from "./tiny-slider/Main.vue";
 import ClassicEditor from "./ckeditor/ClassicEditor.vue";
 
@@ -66,7 +65,6 @@ export default (app) => {
   app.component("Tippy", Tippy);
   app.component("TippyContent", TippyContent);
   app.component("TomSelect", TomSelect);
-  app.component("TomInput", TomInput);
   app.component("LoadingIcon", LoadingIcon);
   app.component("TinySlider", TinySlider);
   app.component("Dropzone", Dropzone);
