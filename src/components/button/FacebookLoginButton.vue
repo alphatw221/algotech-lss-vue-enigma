eff123a6e0a09bc45990fcca1644e394155db6f5
lin <yihsuehlin@accoladeglobal.com>
2023-02-01 17:03:49 +0800

    update

--- src/components/button/FacebookLoginButton.vue
+++ src/components/button/FacebookLoginButton.vue
@@ -5,7 +5,7 @@
             data-size="large" 
             data-button-type="login_with" 
             data-layout="default" 
-            data-auto-logout-link="false" 
+            data-auto-logout-link="true" 
             data-use-continue-as="true" 
             scope="public_profile,email" 
             onlogin="checkLoginState()">
@@ -26,18 +26,36 @@ const route = useRoute();
 const router = useRouter();
 const role = route.path.split('/')[1]
 onMounted(()=>{
+
+    var div = document.createElement( "div" )
+    div.id='fb-root'
+
+    var script = document.createElement( "script" )
+    script.async=true
+    script.defer=true
+    script.crossOrigin="anonymous"
+    script.src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v15.0&appId=967598017063136&autoLogAppEvents=1"
+    script.nonce="sdNz3djI"
+
+    var body = document.getElementsByTagName( "body" )[0]
+    body.insertBefore(script, body.firstChild)
+    body.insertBefore(div, body.firstChild)
+
+
+
+
     //facebook SDK use eval() at backend
-    loadScript("https://connect.facebook.net/en_US/sdk.js",()=>{
-        console.log("FB SDK loaded")
-        window.fbAsyncInit = function() {
-            window.FB.init({
-                appId: import.meta.env.VITE_APP_FB_APP_ID, 
-                cookie: true,
-                xfbml: true,
-                version: "v13.0",
-            });
-        }
-    });
+    // loadScript("https://connect.facebook.net/en_US/sdk.js",()=>{
+    //     console.log("FB SDK loaded")
+    //     window.fbAsyncInit = function() {
+    //         window.FB.init({
+    //             appId: import.meta.env.VITE_APP_FB_APP_ID, 
+    //             cookie: true,
+    //             xfbml: true,
+    //             version: "v13.0",
+    //         });
+    //     }
+    // });
     window.checkLoginState = () => {
         window.FB.getLoginStatus(response => {
             if (response.status === 'connected') {
