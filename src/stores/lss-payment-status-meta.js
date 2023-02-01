c220578dbf9fd41d64ffc74707003f45f368c696
lin <yihsuehlin@accoladeglobal.com>
2023-02-01 10:51:31 +0800

    update

--- src/stores/lss-payment-status-meta.js
+++ src/stores/lss-payment-status-meta.js
@@ -6,8 +6,8 @@ export const usePaymentStatusStore = defineStore("paymentMetaStore", {
 	awaiting_payment: {
 		name: "Awaiting Payment",
 		key:"awaiting_payment",
-		allow_adjust:false,
-		options:[]
+		allow_adjust:true,
+		options:['awaiting_confirm', 'failed', 'expired', 'paid', 'awaiting_refund', 'refunded']
 	},
 
 
@@ -15,42 +15,42 @@ export const usePaymentStatusStore = defineStore("paymentMetaStore", {
 		name: "Awaiting Confirm",
 		key:"awaiting_confirm",
 		allow_adjust:true,
-		options:['awaiting_confirm', 'awaiting_payment', 'failed', 'paid']
+		options:['awaiting_payment', 'failed', 'expired', 'paid', 'awaiting_refund', 'refunded']
 	},
 
 	failed: {
 		name: "Failed",
 		key:"failed",
-		allow_adjust:false,
-		options:[]
+		allow_adjust:true,
+		options:['awaiting_confirm', 'awaiting_payment', 'expired', 'paid', 'awaiting_refund', 'refunded']
 	},
 
 	expired: {
 		name: "Expired",
 		key:"expired",
-		allow_adjust:false,
-		options:[]
+		allow_adjust:true,
+		options:['awaiting_confirm', 'awaiting_payment', 'failed', 'paid', 'awaiting_refund', 'refunded']
 	},
 
 	paid: {
 		name: "Paid",
 		key:"paid",
-		allow_adjust:false,
-		options:[]
+		allow_adjust:true,
+		options:['awaiting_confirm', 'awaiting_payment', 'failed', 'expired', 'awaiting_refund', 'refunded']
 	},
 
 	awaiting_refund: {
 		name: "Awaiting Refund",
 		key:"awaiting_refund",
 		allow_adjust:true,
-		options:['awaiting_refund', 'refunded']
+		options:['awaiting_confirm', 'awaiting_payment', 'failed', 'expired', 'paid', 'refunded']
 	},
 
 	refunded: {
 		name: "refunded",
 		allow_adjust:false,
 		key:"refunded",
-		options:[]
+		options:['awaiting_confirm', 'awaiting_payment', 'failed', 'expired', 'paid', 'awaiting_refund']
 	},
 
     
