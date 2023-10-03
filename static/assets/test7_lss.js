161150ed11b114d15707fbb13e41e925ddd828f0
lin <yihsuehlin@accoladeglobal.com>
2023-10-04 01:11:24 +0800

    update

--- static/assets/test7_lss.js
+++ static/assets/test7_lss.js
@@ -1,1 +1,1 @@
-import{T as t,j as r,b3 as s}from"./lss.js";const i={__name:"test7",setup(c){const e="https://v2login.liveshowseller.com/buyer/test";return(o,_)=>(r(),t(s,{redirect_uri:e}))}};export{i as default};
+import{T as t,j as r,b3 as s}from"./lss.js";const n={__name:"test7",setup(c){const e="https://liveshowseller.net/buyer/test";return(_,a)=>(r(),t(s,{redirect_uri:e}))}};export{n as default};
